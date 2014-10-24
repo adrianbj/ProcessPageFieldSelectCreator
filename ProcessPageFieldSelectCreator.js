5d01d883a614193dc5ca17eda5eb0f6ba8c2526f
adrianbj <adrianbjones@gmail.com>
2014-10-23 17:28:12 -0700

    Add option to exclude select options template from being added to the Add Page shortcut menu. And a couple of minor fixes / tweaks.

--- ProcessPageFieldSelectCreator.js
+++ ProcessPageFieldSelectCreator.js
@@ -1,5 +1,5 @@
 $(window).load(function() {
-    $('#Inputfield_fieldTitle').bind('blur', function(){
+    $('#Inputfield_fieldTitle').bind('keyup change', function(){
         $("#Inputfield_parentTemplate").val($('#Inputfield_fieldTitle').val());
         $("#Inputfield_childTemplate").val($('#Inputfield_fieldTitle').val() + ' ' + $("#Inputfield_childTemplateSuffix").val());
     });
