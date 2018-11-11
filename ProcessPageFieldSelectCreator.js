01a7017e50d6f90522b555152d4d99df67a52da0
adrianbj <adrianbjones@gmail.com>
2018-11-11 12:25:49 -0700

    Add new field to control parent page title (so it doesn't have to be the same as the field label). PHP 7 fix for Option 2 setup. Various cleanup and relabeling of options.

--- ProcessPageFieldSelectCreator.js
+++ ProcessPageFieldSelectCreator.js
@@ -1,6 +1,7 @@
 $(window).load(function() {
-    $('#Inputfield_fieldTitle').bind('keyup change', function(){
-        $("#Inputfield_parentTemplate").val($('#Inputfield_fieldTitle').val());
-        $("#Inputfield_childTemplate").val($('#Inputfield_fieldTitle').val() + ' ' + $("#Inputfield_childTemplateSuffix").val());
+    $('#Inputfield_fieldLabel').bind('keyup change', function(){
+        $("#Inputfield_parentTemplate").val($('#Inputfield_fieldLabel').val());
+        $("#Inputfield_childTemplate").val($('#Inputfield_fieldLabel').val() + ' ' + $("#Inputfield_childTemplateSuffix").val());
+        $("#Inputfield_parentPageTitle").val($('#Inputfield_fieldLabel').val());
     });
 });
