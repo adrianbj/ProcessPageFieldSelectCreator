1d9fd8c27870e0ac66c5cc1624baacfb708f49b5
adrianbj <adrianbjones@gmail.com>
2018-11-14 16:34:28 -0800

    Improve input field type selection. Better session storage of certain selected settings. Other cleanup.

--- ProcessPageFieldSelectCreator.js
+++ ProcessPageFieldSelectCreator.js
@@ -1,15 +1,17 @@
 $(window).load(function() {
     $('#Inputfield_fieldLabel').bind('keyup change', function() {
-    	var field_label = $('#Inputfield_fieldLabel').val();
-    	var plural = pluralize(field_label);
-    	var singular = pluralize(field_label, 1);
-	    $('#Inputfield_parentTemplate').val(plural);
-	    $('#Inputfield_childTemplate').val(singular);
-	    $('#Inputfield_parentPageTitle').val(plural);
+        var field_label = $('#Inputfield_fieldLabel').val();
+        var plural = pluralize(field_label);
+        var singular = pluralize(field_label, 1);
+        $('#Inputfield_parentTemplate').val(plural);
+        $('#Inputfield_childTemplate').val(singular);
+        $('#Inputfield_parentPageTitle').val(plural);
     });
+    // if parent and child template names are the same, set child to blank so user has to manually enter something different
+    // this could occur if the field name is set to something like "sheep" where plural and singular is the same
     $('#Inputfield_fieldLabel').bind('blur', function() {
-    	if($('#Inputfield_parentTemplate').val() === $('#Inputfield_childTemplate').val()) {
-    		$('#Inputfield_childTemplate').val('');
-    	}
+        if($('#Inputfield_parentTemplate').val() === $('#Inputfield_childTemplate').val()) {
+            $('#Inputfield_childTemplate').val('');
+        }
     });
 });
